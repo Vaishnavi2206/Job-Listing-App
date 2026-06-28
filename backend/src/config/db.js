4f9007d87ee266ca0100655501545f5c63644405
v-vrawool@e-emphasys.com <v-vrawool@e-emphasys.com>
2026-06-28 17:09:47 +0530

    Dockerize application with nginx reverse proxy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-console.log(process.env.DB_PASSWORD, "password")
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
